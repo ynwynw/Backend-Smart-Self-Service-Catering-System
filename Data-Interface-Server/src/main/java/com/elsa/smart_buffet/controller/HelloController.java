19dcf83d5572e1b5a03009ba0b5611217555394d
“Elsa-zlt” <2903250242@qq.com>
2022-06-18 11:53:32 +0800

    Server

--- Data-Interface-Server/src/main/java/com/elsa/smart_buffet/controller/HelloController.java
+++ Data-Interface-Server/src/main/java/com/elsa/smart_buffet/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+//@CrossOrigin
 public class HelloController {
 
     @RequestMapping("/hello")
